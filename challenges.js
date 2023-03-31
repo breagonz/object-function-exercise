22819fca271200598ac3c448aa1194435f8f3960
Breanna Gonzales <breagonz@Breannas-Air.lan>
2023-03-30 19:31:54 -0700

    added

--- challenges.js
+++ challenges.js
@@ -1,13 +1,13 @@
 // Create a variable called myName and assign it your name as a string.
-
+let myNmae = "Breanna"
 // Create an array called numbers and add five numbers to it.
-
+let number = ["1", "2", "3"]
 // Create a variable called myAge and assign it your age as a number.
-
+let myAge = "28"
 // Create an object called person with properties name, age, and gender.
-
+let person = {name:"Jim", age:"30", gender:"M"}
 // Create a variable called isRaining and assign it a boolean value based on whether it is currently raining or not.
-
+let isRaining = true 
 // Create a function called greet that takes a name as a parameter and returns a greeting message using template literals.
 
 // Create a function called multiply that takes two numbers as parameters and returns their product.
